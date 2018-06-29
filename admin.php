62af0e14571424c7a7aab5b87f3143c425943904
Siddharth Ashok <siddharthashokg@gmail.com>
2018-06-29 21:30:47 +0530

    redy for demo

--- admin.php
+++ admin.php
@@ -7,21 +7,23 @@ include("header.php"); ?>
   <div class="grid-container">
     <div class="grid-x grid-padding-x">
       <div class="large-12 cell">
-        <a href="#">Pilots</a>
-        <a href="#">Operators</a>
-        <a href="#">Manufacturer</a>
+        <ul class="menu">
+          <li><a href="#" class="pilot active"><span>Pilots</span></a></li>
+          <li><a href="#" class="operator"><span>Operators</span></a></li>
+          <li><a href="#" class="manufacturer"><span>Manufacturer</span></a></li>
+        </ul>
       </div>
     </div>
   </div>
 </div>
 
 <div class="page-dashboard">
-  <section id="all-applications">
+  <section id="all-applications-admin">
     <div class="grid-container">
       <div class="grid-x grid-padding-x">
         <div class="large-12 cell">
 
-          <div class="search">
+          <div class="admin-search-wrap">
             <form class="admin-search-form" action="" method="post">
               <input type="text" name="" value="" class="admin-search" placeholder="Search">
               <button type="submit" name="button" class="admin-search-button"> <img src="img/search.svg" alt=""> </button>
@@ -32,8 +34,8 @@ include("header.php"); ?>
             <div class="application">
               <div class="details">
                 <div class="wrap">
-                  <h3>Lucknow</h3>
-                  <p>Shivgarh Resorts</p>
+                  <h3>Siddharth Ashok</h3>
+                  <p>Parrot Bebop 2 FPV</p>
                 </div>
               </div>
               <div class="go-to-location">
@@ -44,20 +46,89 @@ include("header.php"); ?>
             <div class="application">
               <div class="details">
                 <div class="wrap">
-                  <h3>Lucknow</h3>
-                  <p>Shivgarh Resorts</p>
+                  <h3>Siddharth Shetty</h3>
+                  <p>Parrot Bebop 2 FPV</p>
                 </div>
               </div>
               <div class="go-to-location">
                 <a href="#" class="button"><img src="img/view.svg" alt=""></a>
               </div>
             </div>
+
+            <div class="application">
+              <div class="details">
+                <div class="wrap">
+                  <h3>Siddharth Shetty</h3>
+                  <p>Parrot Bebop 2 FPV</p>
+                </div>
+              </div>
+              <div class="go-to-location">
+                <a href="#" class="button"><img src="img/view.svg" alt=""></a>
+              </div>
+            </div>
+
+            <div class="application">
+              <div class="details">
+                <div class="wrap">
+                  <h3>Abhishek Prabhu</h3>
+                  <p>Parrot Bebop 2 FPV</p>
+                </div>
+              </div>
+              <div class="go-to-location">
+                <a href="#" class="button"><img src="img/view.svg" alt=""></a>
+              </div>
+            </div>
+
+            <div class="application">
+              <div class="details">
+                <div class="wrap">
+                  <h3>Mereeva Alphanso</h3>
+                  <p>Parrot Bebop 2 FPV</p>
+                </div>
+              </div>
+              <div class="go-to-location">
+                <a href="#" class="button"><img src="img/view.svg" alt=""></a>
+              </div>
+            </div>
+
+            <div class="application">
+              <div class="details">
+                <div class="wrap">
+                  <h3>Cedan Misquith</h3>
+                  <p>Parrot Bebop 2 FPV</p>
+                </div>
+              </div>
+              <div class="go-to-location">
+                <a href="#" class="button"><img src="img/view.svg" alt=""></a>
+              </div>
+            </div>
+
           </div>
 
         </div>
       </div>
     </div>
   </section>
+
+  <div class="page-navigation">
+      <div class="grid-container">
+        <div class="grid-x grid-padding-x">
+          <div class="large-12 cell">
+            <ul class="menu">
+              <li><a href="#"><span class="wrap"><img src="img/page-start.svg" alt=""></span></a></li>
+              <li><a href="#"><span class="wrap"><img src="img/page-previous.svg" alt=""></span></a></li>
+              <li><a href="#" class="active"><span class="wrap">1</span></a></li>
+              <li><a href="#"><span class="wrap">2</span></a></li>
+              <li><a href="#"><span class="wrap">3</span></a></li>
+              <li><a href="#"><span class="wrap"><img src="img/page-next.svg" alt=""></span></a></li>
+              <li><a href="#"><span class="wrap"><img src="img/page-end.svg" alt=""></span></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+  </div>
+
+
 </div>
 
 <?php include("footer.php");
---
+++ view-application.php
@@ -0,0 +1,112 @@
+<?php
+$dashboard = "true";
+include("header.php"); ?>
+
+<div id="view-application">
+  <div class="grid-container">
+    <div class="grid-x grid-padding-x">
+      <div class="large-12 cell">
+        <label class="page-heading">Review your application</label>
+      </div>
+    </div>
+  </div>
+
+
+
+  <div id="application-preview">
+    <div class="grid-container">
+      <div class="grid-x grid-padding-x">
+
+        <div class="large-12 cell">
+
+          <div class="question">
+            <h6>Name of applicant in full:</h6>
+            <p>Siddharth Ashok</p>
+          </div>
+
+          <div class="question">
+            <h6>Parentage</h6>
+            <p>Ashok Sharma</p>
+          </div>
+
+          <div class="question">
+            <h6>Present address</h6>
+            <p>Mountainview colony, 40002, Panaji,
+            Goa, India. Near Krishna Temple.</p>
+          </div>
+
+          <div class="question">
+            <h6>Resident of India?</h6>
+            <p>Yes, I am a resident of india</p>
+          </div>
+
+          <div class="question">
+            <h6>Permanent address</h6>
+            <p>Mountainview colony, 40002, Panaji,
+            Goa, India. Near Krishna Temple.</p>
+          </div>
+
+          <div class="question">
+            <h6>Nationality</h6>
+            <p>Indian</p>
+          </div>
+
+          <div class="question">
+            <h6>Date of birth</h6>
+            <p>06/09/1987</p>
+          </div>
+
+          <div class="question">
+            <h6>Place of birth</h6>
+            <p>Mountainview colony, 40002, Panaji,
+            Goa, India. Near Krishna Temple.</p>
+          </div>
+
+          <div class="question">
+            <h6>Profession/ occupation after the age of 18 years</h6>
+            <p>Developer at Google</p>
+            <p>Developer at Apple, Developer at GrandWorks.</p>
+          </div>
+
+          <div class="question">
+            <h6>Particulars of places, with full
+  address, where the applicant has resided for more than a year during
+  the preceding five years</h6>
+            <p>Mountainview colony, 40002, Panaji,
+            Goa, India. Near Krishna Temple.</p>
+          </div>
+
+          <div class="question">
+            <h6>Has the applicant ever been arrested, prosecuted, kept under detention, or convicted by a court? Give details:</h6>
+            <p>No</p>
+          </div>
+
+          <div class="question">
+            <h6>Training Certificate</h6>
+            <p>Siddharth_certificate.pdf</p>
+          </div>
+
+          <div class="question">
+            <h6>Drone Category</h6>
+            <p>Micro. Greater than 250 grams and less than or equal to 2 kg.</p>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="view-application-footer">
+    <div class="grid-container">
+      <div class="grid-x grid-padding-x">
+        <div class="large-12 cell">
+          <a href="apply-for-pilot-step-2.php" class="back"> <img src="img/back.svg" alt=""> Go back to previous step</a>
+          <a class="button button-accept">Accept Application</a>
+          <a class="button button-deny">Deny Application</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php include("footer.php");
