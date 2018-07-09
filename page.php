37c128cc6a0ae00072b0bcdd66f752812e960485
Siddharth Ashok <siddharthashokg@gmail.com>
2018-07-09 16:37:22 +0530

    readme

---
+++ page.php
@@ -0,0 +1,22 @@
+<?php include("header.php"); ?>
+
+<div class="single-page">
+  <div class="grid-container">
+    <div class="grid-x grid-padding-x">
+      <div class="large-12 cell">
+        <h2>This is page title</h2>
+
+        <p>Cupcake ipsum dolor sit amet chocolate bar. Bear claw candy canes cake pudding ice cream I love dragée oat cake. Applicake pie halvah. Sesame snaps cupcake ice cream I love I love cookie I love. Soufflé jelly-o donut cupcake. I love pastry marshmallow marzipan applicake I love carrot cake. Oat cake icing oat cake. Toffee gummi bears dragée I love toffee I love icing I love brownie. Toffee biscuit gummi bears jelly beans pie.</p>
+
+        <p>Lemon drops jelly candy I love chupa chups I love. Tootsie roll sweet gummies. Chocolate cookie lollipop gummi bears wafer candy canes. Gummi bears dessert cupcake. Tart powder jelly beans pie. Applicake I love croissant.</p>
+
+        <p>Topping cookie powder powder cake. Wafer gingerbread applicake sweet cake ice cream soufflé sweet. Icing sugar plum oat cake toffee icing. I love candy canes I love dessert sweet cotton candy gummies. Wafer powder chocolate marshmallow I love tootsie roll I love macaroon. Fruitcake apple pie ice cream carrot cake jelly wypas.</p>
+
+        <p>I love icing wafer I love. Bonbon dessert cotton candy dessert pie brownie cupcake croissant sweet roll. Cake jelly beans tart marshmallow wafer. Dragée chupa chups bear claw I love cupcake danish. I love gingerbread soufflé faworki muffin I love pudding gingerbread I love. Donut tootsie roll chocolate cotton candy pastry marshmallow brownie cotton candy. Marshmallow cookie wafer chocolate cake.</p>
+
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php include("footer.php");
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Notes
+
+## General
+
+- To use same header and footer on all pages we have included header.php & footer.php in each pages
