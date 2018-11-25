070380089c9cbb6e00a0fa546e2ede765742e141
Siddharth Ashok <siddharthashokg@gmail.com>
2018-11-25 11:01:38 +0530

    Links index

---
+++ links.php
@@ -0,0 +1,55 @@
+<?php include("header.php"); ?>
+
+<div class="grid-container">
+  <div class="grid-x grid-padding-x">
+    <div class="large-12 cell">
+      <h3>Home</h3>
+      <a href="index.php" target="_blank">Home</a><br>
+
+      <h3>Other Pages</h3>
+
+      <a href="verified.php" target="_blank">Verified</a><br>
+      <a href="thanks.php" target="_blank">Thanks</a><br>
+      <a href="fail.php" target="_blank">Failed</a><br>
+      <a href="registered.php" target="_blank">Registered</a><br>
+      <a href="blog.php" target="_blank">Blog</a><br>
+      <a href="page.php" target="_blank">Single Page Template</a><br>
+      <a href="know-requirements.php" target="_blank">Know Reequirements</a><br>
+
+      <h3>Signup-login</h3>
+
+      <a href="register.php" target="_blank">Register</a><br>
+      <a href="login.php" target="_blank">Login</a><br>
+
+      <h3>Dashboard</h3>
+
+      <a href="dashboard-no-data.php" target="_blank">Dashboard</a><br>
+      <a href="drone-profile.php" target="_blank">Drone Profile</a><br>
+
+      <h3>Admin</h3>
+      <a href="admin.php" target="_blank">Admin</a><br>
+      <a href="view-application.php" target="_blank">View Application</a><br>
+
+      <h3>Test Locations</h3>
+      <a href="test-locations.php" target="_blank">Test Locations</a><br>
+
+      <h3>Apply For</h3>
+      <a href="apply-for.php" target="_blank">Apply For</a><br>
+      <a href="apply-for-pilot.php" target="_blank">Step 1</a><br>
+      <a href="apply-for-pilot-step-2.php" target="_blank">Step 2</a><br>
+      <a href="apply-for-pilot-step-3.php" target="_blank">Step 3</a><br>
+
+      <h3>Apply For Permission</h3>
+      <a href="apply-for-permission-step-1.php" target="_blank">Step 1 - Fill form</a><br>
+      <a href="apply-for-permission-map.php" target="_blank">Step 2 - Choose area</a><br>
+      <a href="apply-for-permission-step-review.php" target="_blank">Step 2 - Application review</a><br>
+      <p><strong>Bulk permission</strong></p>
+      <a href="apply-for-permission-bulk.php" target="_blank">Step 1</a><br>
+      <a href="apply-for-permission-bulk-step-2.php" target="_blank">Step 2</a><br>
+
+
+    </div>
+  </div>
+</div>
+
+<?php include("footer.php");
